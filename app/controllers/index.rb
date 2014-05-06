72199d8d656e6fe259213ea873b10ae6972f4336
charlotterea <JWu@Johnnys-MacBook-Pro.local>
2014-05-06 16:41:04 -0700

    adds projects homepage view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,34 @@
-get '/' do
+get '/' do	
+	redirect to '/projects'
+end
 
-	
+get '/projects' do   #  projects#index
+	@projects = Project.all
   erb :index
 end
+
+get '/projects/new' do # projects#new
+	
+	erb :new
+end
+
+post '/projects' do # projects#create
+
+end
+
+get '/projects/:id' do # projects#show
+
+	erb :show
+end
+
+get '/projects/:id/edit' do # projects#edit
+
+	erb :edit
+end
+
+delete '/projects/:id' do # projects#destroy
+
+
+end
+
 
