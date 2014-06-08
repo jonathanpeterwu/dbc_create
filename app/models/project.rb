7122d706326d399e2cb48ff0b0e3b54f775900a2
Katherine Hayden <katherineimogene@gmail.com>
2014-06-07 18:42:56 -0700

    adds filtering methods

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,33 @@
 class Project < ActiveRecord::Base
 
-	def self.sort_by(param)
-		# do some stuff
-		
+# opts will be a hash: {sort_by: category, value: category value, year: year }.
+# year will probably not be there in some cases..
+# eg, to look up project by the Golden Bears, {sort_by: cohort, value: "Golden Bears", year: 2014}
+	class << self
+		def sort_by(opts)
+			self.send("sort_by_#{opts[:sort_by]}",opts)
+		end
+
+		def sort_by_location(opts)
+			self.find(location: opts[:value])
+		end
+
+		def sort_by_tags(opts)
+			# assumes value is an array of tags, eg ["JavaScript", "single page app"]
+			# 
+			opts[:value].map{ |tag| self.find(tag: tag) }
+		end
+
+		def sort_by_cohort(opts)
+			result = self.where(opts[:value])
+			result = result.sort_by_year(opts) if opts[:year]
+		end
+
+		def sort_by_year(opts)
+			self.find(year: opts[:year].year)
+		end
 	end
+
 end
 
 # 1. Full text search with pg_search gem
