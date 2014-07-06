4413c417b56974aba35c6f0a81b328669f01eb26
katherineimogene <katherineimogene@gmail.com>
2014-07-05 20:59:26 -0700

    seeds method looks for csv file in same directory

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 # Project.create(name: "test5", description: "goodbye5", source_url: "http://www.google.com", production_url: "http://www.github.com")
 # Project.create(name: "sf", description: "sf goodbye", source_url: "http://www.google.com", production_url: "http://www.github.com", location: "San Francisco")
 
+AppRoot = File.expand_path(File.dirname(__FILE__))
 
-f = File.open("/Users/JWu/Dropbox/Coding/DBC/DBC- Phase4/final_projects/db/seed_data.csv", "r")
+f = File.open(File.join(AppRoot, "seed_data.csv"), "r")
 f.each_line do |line|
 	project = line.split(',')
 	Project.create(name: project[1], description: project[6], source_url: project[4], production_url: project[3], location: project[-1], tags: "final_project", image_url: project[5], team_member: project[-2])
