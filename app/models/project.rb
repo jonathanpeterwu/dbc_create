3d1718bcef40f277277f67d7330a2b19c2b30ab3
katherineimogene <katherineimogene@gmail.com>
2014-07-05 20:39:04 -0700

    adds method to validate links. adds http if not included so links don't break

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,13 @@
 class Project < ActiveRecord::Base
 
   def validate_links
-
+    valid_links = [self.production_url, self.source_url].map do |link|
+       unless link.match(/^https?:\/\//)
+        "https://" + link
+      end
+    end
+    self.production_url, self.source_url = valid_links[0], valid_links[1]
   end
+
 end
 
-
-
-      t.string :production_url
-      t.string :source_url
